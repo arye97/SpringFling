47e9860bd7afc5a9dae266ff78be5b9b298cb01e
Ryan Franks <ref48@uclive.ac.nz>
2020-11-17 16:02:28 +1300

    Initial Commit

---
+++ springfling/src/main/java/com/example/springfling/SpringflingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringflingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringflingApplication.class, args);
+	}
+
+}
---
+++ springfling/src/test/java/com/example/springfling/SpringflingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringflingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
